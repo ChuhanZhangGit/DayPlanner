9181b674537c5cfaa45c140255dcf97a9e5e5106
Chuhan <zhangchuhan1995@gmail.com>
2019-09-04 16:58:36 -0400

    Create app drawer for calendar view

--- app/src/main/java/com/example/hourlyplanner/taskslot/SlotsListActivity.java
+++ app/src/main/java/com/example/hourlyplanner/taskslot/SlotsListActivity.java
@@ -2,22 +2,29 @@ package com.example.hourlyplanner.taskslot;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.LinearLayout;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.widget.Toolbar;
 
-import androidx.annotation.NavigationRes;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.hourlyplanner.R;
-import com.example.hourlyplanner.data.PlannerRepository;
-import com.example.hourlyplanner.data.local.PlannerDataBase;
-
+import com.google.android.material.appbar.AppBarLayout;
 
 
 public class SlotsListActivity extends AppCompatActivity {
+
+    private AppBarLayout appBarLayout;
+
+    private CalendarView calendarView;
+
+    private boolean isExpanded = false;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
@@ -27,7 +34,17 @@ public class SlotsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
-        setupToolBar();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        appBarLayout = findViewById(R.id.app_bar_layout);
+        calendarView = findViewById(R.id.calendar_view);
+
+
+        setupPickDateButton();
 
         SlotsListFragment slotsListFragment = (SlotsListFragment) getSupportFragmentManager().
                 findFragmentById(R.id.frag_task_list);
@@ -36,15 +53,24 @@ public class SlotsListActivity extends AppCompatActivity {
             slotsListFragment = SlotsListFragment.newInstance();
         }
 
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
-    private void setupToolBar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+    private void setupPickDateButton() {
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        LinearLayout pickDateButton = findViewById(R.id.pick_date_button);
+        pickDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isExpanded = !isExpanded;
+                appBarLayout.setExpanded(isExpanded,true);
+            }
+        });
     }
 
     @Override
